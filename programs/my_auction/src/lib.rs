f6e5f5b3dd77bfd5489eef10c2946077ff1938ec
F.S. Kroes <fskroes@users.noreply.github.com>
2022-07-31 12:36:50 +0200

    Setup Auction and initialising state

--- programs/my_auction/src/lib.rs
+++ programs/my_auction/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::TokenAccount;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -12,4 +13,44 @@ pub mod my_auction {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(auction_duration: u64, initializer_amount: u64)]
+pub struct Initialize<'info> {
+    
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut, signer)]
+    pub exhibitor: AccountInfo<'info>,
+    #[account(
+        mut,
+        constraint = exhibitor_nft_token_account.amount == 1
+    )]
+    pub exhibitor_nft_token_account: Account<'info, TokenAccount>,
+    pub exhibitor_nft_temp_account: Account<'info, TokenAccount>,
+    pub exhibitor_ft_receiving_account:Account<'info, TokenAccount>,
+    
+    #[account(zero)]
+    pub escrow_account: Box<Account<'info, Auction>>,
+
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub system_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub clock: Sysvar<'info, Clock>,
+    
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_program: AccountInfo<'info>,
+}
+
+#[account]
+pub struct Auction {
+
+    pub exhibitor_pubkey: Pubkey,
+    pub exhibiting_nft_temp_pubkey: Pubkey,
+    pub exhibitor_ft_receiving_pubkey: Pubkey,
+    
+    pub highest_bidder_pubkey: Pubkey,
+    pub highest_bidder_ft_temp_pubkey: Pubkey,
+    pub highest_bidder_ft_returning_pubkey: Pubkey,
+
+    pub price: u64,
+    pub end_at: i64,
+
+}
--- tests/my_auction.ts
+++ tests/my_auction.ts
@@ -3,14 +3,14 @@ import { Program } from "@project-serum/anchor";
 import { MyAuction } from "../target/types/my_auction";
 
 describe("my_auction", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.MyAuction as Program<MyAuction>;
 
+
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    //
   });
 });
